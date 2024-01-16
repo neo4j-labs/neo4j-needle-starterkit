28c2966c8ad846b06de27f77e81d5d072894ccc3
msenechal <morgan.senechal@neo4j.com>
2024-01-16 10:24:20 +0000

    Fixed name for broken link in labs

--- docs/server.js
+++ docs/server.js
@@ -3,5 +3,5 @@ const express = require('express')
 const app = express()
 const version = "1.0"
 app.use(express.static('./build/site'))
-app.get('/', (req, res) => res.redirect('needle-starter-kit/' + version))
+app.get('/', (req, res) => res.redirect('neo4j-needle-starterkit/' + version))
 app.listen(8000, () => console.log('📘 http://localhost:8000'))
