fef43e5b6826642752e5445b7f7d347e6d590cca
msenechal <morgan.senechal@neo4j.com>
2024-02-08 23:38:19 +0000

    tmp fix

--- src/landingPage/categories/Templates.tsx
+++ src/landingPage/categories/Templates.tsx
@@ -5,7 +5,7 @@ import StarterKitImgLight from '../../assets/img/StarterKitImg-light.png';
 import EcommerceImgDark from '../../assets/img/Ecommerce-dark.png';
 import EcommerceImgLight from '../../assets/img/Ecommerce-light.png';
 import MovieImgDark from '../../assets/img/WIPImg.png';
-import MovieImgLight from '../../assets/img/Movie-light.png';
+import MovieImgLight from '../../assets/img/WIPImg.png';
 import WIPImg from '../../assets/img/WIPImg.png';
 
 import { useContext } from 'react';
