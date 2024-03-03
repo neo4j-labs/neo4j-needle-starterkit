e04c016e3f1d22701e41f5c07b7fda6c5a00076a
Morgan Senechal <morgan@neo4j.com>
2024-03-03 21:07:55 +0100

    Fixed format + lint

--- src/templates/cybersecurity/Home.tsx
+++ src/templates/cybersecurity/Home.tsx
@@ -11,7 +11,7 @@ import {
   Widget,
 } from '@neo4j-ndl/react';
 import { MagnifyingGlassIconOutline, InformationCircleIconOutline } from '@neo4j-ndl/react/icons';
-import { createColumnHelper, getCoreRowModel, getSortedRowModel, useReactTable, Table } from '@tanstack/react-table';
+import { createColumnHelper, getCoreRowModel, getSortedRowModel, useReactTable } from '@tanstack/react-table';
 
 import productsData from './assets/networkimpact.json';
 import NoGraphImg from '../shared/assets/NoData.png';
@@ -25,8 +25,6 @@ type NetworkImpact = {
   Version: string;
   Status: string;
 };
-
-
 
 const columnHelper = createColumnHelper<NetworkImpact>();
 
@@ -105,20 +103,14 @@ export default function Home() {
 
   return (
     <>
-      <Header
-        title='CyberSecurity'
-        navItems={[]}
-        useNeo4jConnect={false}
-        userHeader={false}
-      />
+      <Header title='CyberSecurity' navItems={[]} useNeo4jConnect={false} userHeader={false} />
 
       <div className='landing-page n-bg-palette-neutral-bg-default'>
-        <form
-          className={`search-bar ${isSearchInitiated ? 'top' : 'center'}`}
-          onSubmit={handleSearch}
-          
-        >
-          <div onTransitionEnd={displayResult} className={`text-input-container ${isSearchInitiated ? 'search-initiated' : ''}`}>
+        <form className={`search-bar ${isSearchInitiated ? 'top' : 'center'}`} onSubmit={handleSearch}>
+          <div
+            onTransitionEnd={displayResult}
+            className={`text-input-container ${isSearchInitiated ? 'search-initiated' : ''}`}
+          >
             <TextInput
               type='text'
               value={searchQuery}
@@ -134,55 +126,55 @@ export default function Home() {
             />
           </div>
         </form>
-          <Widget
-            className='n-bg-palette-neutral-bg-weak min-h-[60%] min-w-[60%] flex flex-col search-result'
-            header=''
-            isElevated={true}
-            id='search-result'
-          >
-            <Flex flexDirection='column' justifyContent='space-between'>
-              <div>
-                <Tabs size='large' fill='underline' onChange={(e) => setActiveTab(e)} value={activeTab}>
-                  <Tabs.Tab tabId={0}>Table</Tabs.Tab>
-                  <Tabs.Tab tabId={1}>Graph</Tabs.Tab>
-                </Tabs>
-                <Flex className='p-8'>
-                  {activeTab === 0 ? (
-                    <DataGrid<NetworkImpact>
-                      isResizable={false}
-                      tableInstance={table}
-                      isKeyboardNavigable={false}
-                      styling={{
-                        zebraStriping: true,
-                        borderStyle: 'none',
-                        headerStyle: 'filled',
-                      }}
-                      components={{
-                        Navigation: null,
-                      }}
-                    />
-                  ) : (
-                    <Flex flexDirection='row'>
-                      <img src={NoGraphImg} className='p-12' />
-                      <Flex gap='8'>
-                        <Typography variant='h1'>WIP Screen</Typography>
-                        <Typography variant='body-medium'>
-                          <p>Ideally this would display a graph of the network/search result</p>
-                          <p>
-                            This would be interactive and allow user to explore the network to make a proper impact
-                            analysis
-                          </p>
-                        </Typography>
-                      </Flex>
+        <Widget
+          className='n-bg-palette-neutral-bg-weak min-h-[60%] min-w-[60%] flex flex-col search-result'
+          header=''
+          isElevated={true}
+          id='search-result'
+        >
+          <Flex flexDirection='column' justifyContent='space-between'>
+            <div>
+              <Tabs size='large' fill='underline' onChange={(e) => setActiveTab(e)} value={activeTab}>
+                <Tabs.Tab tabId={0}>Table</Tabs.Tab>
+                <Tabs.Tab tabId={1}>Graph</Tabs.Tab>
+              </Tabs>
+              <Flex className='p-8'>
+                {activeTab === 0 ? (
+                  <DataGrid<NetworkImpact>
+                    isResizable={false}
+                    tableInstance={table}
+                    isKeyboardNavigable={false}
+                    styling={{
+                      zebraStriping: true,
+                      borderStyle: 'none',
+                      headerStyle: 'filled',
+                    }}
+                    components={{
+                      Navigation: null,
+                    }}
+                  />
+                ) : (
+                  <Flex flexDirection='row'>
+                    <img src={NoGraphImg} className='p-12' />
+                    <Flex gap='8'>
+                      <Typography variant='h1'>WIP Screen</Typography>
+                      <Typography variant='body-medium'>
+                        <p>Ideally this would display a graph of the network/search result</p>
+                        <p>
+                          This would be interactive and allow user to explore the network to make a proper impact
+                          analysis
+                        </p>
+                      </Typography>
                     </Flex>
-                  )}
-                </Flex>
-              </div>
-              <div className='text-center'>
-                <>Results for "{searchQuery}"</>
-              </div>
-            </Flex>
-          </Widget>
+                  </Flex>
+                )}
+              </Flex>
+            </div>
+            <div className='text-center'>
+              <>Results for "{searchQuery}"</>
+            </div>
+          </Flex>
+        </Widget>
       </div>
     </>
   );
