0e321f271e7ab71a2e9c5cbdbb34184f3477fd4f
Morgan Senechal <morgan@neo4j.com>
2024-03-04 16:21:55 +0100

    vite-spa

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import packageJson from './package.json';
+import vitePluginSingleSpa from 'vite-plugin-single-spa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), vitePluginSingleSpa({
+    type: 'root'
+    })],
   define: {
     'import.meta.env.PACKAGE_VERSION': JSON.stringify(packageJson.version),
   },
