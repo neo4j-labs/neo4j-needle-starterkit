3eb1139a00acb50a9fc74d0cb7aaaa3c0cead6e1
Morgan Senechal <morgan@neo4j.com>
2024-03-04 17:41:12 +0100

    Reverted

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import packageJson from './package.json';
-import vitePluginSingleSpa from 'vite-plugin-single-spa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    vitePluginSingleSpa({
-      type: 'mife',
-      serverPort: 80,
-      spaEntryPoints: 'src/spa.tsx',
-    }),
-  ],
+  plugins: [react()],
   define: {
     'import.meta.env.PACKAGE_VERSION': JSON.stringify(packageJson.version),
   },
