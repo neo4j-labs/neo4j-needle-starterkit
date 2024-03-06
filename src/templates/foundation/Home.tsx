512df11076ba654b2cc5cbaae5ade0a803f21e86
Morgan Senechal <morgan@neo4j.com>
2024-03-06 21:11:06 +0100

    2nd stab at documentation

--- src/templates/foundation/Home.tsx
+++ src/templates/foundation/Home.tsx
@@ -1,7 +1,7 @@
 import Header from '../shared/components/Header';
 import PageLayout from './Layout/PageLayout';
 
-export default function QuickStarter() {
+export default function Home() {
   return (
     <div>
       <Header title={'StarterKit'} navItems={[]} useNeo4jConnect={false} userHeader={true} />
